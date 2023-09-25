c3bc5865a49e587408857bd85c73dac72ad7c314
iqbal kang <iqbal.kang.cohort225@gmail.com>
2023-09-24 17:47:43 -0700

    before adding EntityFrameWork Core

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -1,25 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Entities
 {
     public class Person
     {
+        [Key]
         public Guid PersonId { get; set; }
 
+        [StringLength(40)]
         public string? PersonName { get; set; }
 
+        [StringLength(40)]
         public string? Email {  get; set; }
 
+        [StringLength(10)]
         public string? Gender { get; set; }
 
         public DateTime? DateOfBirth { get; set; }
 
         public Guid? CountryId { get; set; }
 
+        [StringLength(200)]
         public string? Address { get; set; }
 
         public bool ReceiveNewsletter {  get; set; }
